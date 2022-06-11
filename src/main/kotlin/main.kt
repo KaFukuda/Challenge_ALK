9fe0565d5dbdbb4673e52e1840995440dbef6d88
Karina Fukuda <karina.fukuda@mercadolivre.com>
2022-06-11 19:46:13 -0300

    initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,18 @@
+fun main() {
+println("Hello world")
+}
+
+data class ParkingSpace( var vehicle: Vehicle)
+
+data class Parking(val vehicles : MutableSet<Vehicle>)
+
+data class Vehicle(val plate: String){
+    override fun equals(other: Any?): Boolean {
+        if (other is Vehicle) {
+            return this.plate == other.plate
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int  = this.plate.hashCode()
+}
