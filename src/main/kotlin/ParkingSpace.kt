4d3cfd2ccb2999e877a7634577b0ce29dc6882c4
Karina Fukuda <karina.fukuda@mercadolivre.com>
2022-06-14 20:47:41 -0300

    REFACTOR: code reading improvement

--- src/main/kotlin/ParkingSpace.kt
+++ src/main/kotlin/ParkingSpace.kt
@@ -1,43 +1,40 @@
-import java.util.*
-import kotlin.NoSuchElementException
 import kotlin.math.ceil
 
-data class ParkingSpace(var parking: Parking) {
+const val HOURS_FIXED: Int = 120
+
+data class ParkingSpace(
+    var parking: Parking) {
 
     fun checkOutVehicle(plate: String) {
         try {
-            val vehicle = parking.vehicles.first { v ->
-                v.plate == plate
-            }
+            val vehicle = parking.vehicles.first { validate -> validate.plate == plate }
 
             val fee = calculateFee(vehicle.vehicleType, vehicle.parkedTime, vehicle.discountCard != null)
 
-            parking.total = Pair(parking.total.first.plus(1), parking.total.second.plus(fee))
-
+            parking.reportData = Pair(parking.totalVehicles.plus(1), parking.totalCash.plus(fee))
             onSuccess(fee)
 
             parking.vehicles.remove(vehicle)
+
         } catch (e: Exception) {
             onError("Sorry, the check-out failed")
-            /*if (e is NoSuchElementException) {
-                //nenhum veiculo encontrado
-            }*/
         }
     }
 
-    private fun calculateFee(type: VehicleType, parkedTime: Long, hasDiscountCard: Boolean): Int {
-        var cobranca =
+    private fun calculateFee(
+        type: VehicleType,
+        parkedTime: Long,
+        hasDiscountCard: Boolean): Int
+    {
+        val charges =
             when {
-                parkedTime <= 120 -> {
-                    type.fee
-                }
+                parkedTime <= HOURS_FIXED -> type.fee
                 else -> {
-                    val diferenca = parkedTime - 120
-                    type.fee + (ceil(diferenca.toDouble() / 15) * 5)
+                    val diff = parkedTime - HOURS_FIXED
+                    type.fee + (ceil(diff.toDouble() / 15) * 5)
                 }
             }
-
-        return (if (hasDiscountCard) cobranca.toDouble() - (cobranca.toDouble() * 0.15) else cobranca).toInt()
+        return (if (hasDiscountCard) charges.toDouble() - (charges.toDouble() * 0.15) else charges).toInt()
     }
 
     private fun onSuccess(fee: Int) {
