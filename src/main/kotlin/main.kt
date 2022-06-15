1de5d65bab9dca8f993c7bd17f1c728c29fb69dd
joseqfonseca <jose.qfonseca@mercadolivre.com>
2022-06-15 14:59:56 -0300

    REFACTOR: clean code

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -2,9 +2,10 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 fun main() {
-    val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.ENGLISH)
-    sdf.parse("2022/06/14 13:00")
-    val cal = sdf.calendar
+    //testing fee with other date
+    /*val sdf = SimpleDateFormat("yyyy/MM/dd HH:mm", Locale.ENGLISH)
+    sdf.parse("2022/06/15 12:00")
+    val cal = sdf.calendar*/
 
     val parking = Parking(20, vehicles = mutableSetOf())
 
@@ -55,5 +56,5 @@ fun main() {
     //println(parking.vehicles.size)
 
     //testing vehicle totals and earnings report
-    //parking.getAdminReport()
+    parking.getAdminReport()
 }
